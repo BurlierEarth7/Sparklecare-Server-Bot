6fa83c181d93df042c278904c86dca04fa5c3a89
BurlierEarth7 <j.tollefson@outlook.com>
2024-03-09 21:39:25 -0700

    Add status and activity to bot

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,13 @@
 // Imports
-const { Events } = require("discord.js");
+const { Events, ActivityType } = require("discord.js");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
+    client.user.setStatus("online");
+    client.user.setActivity("Sparklecare | Type / for a list of commands!", { type: ActivityType.Watching });
+
   }, // End of execute
 }; // End of exports

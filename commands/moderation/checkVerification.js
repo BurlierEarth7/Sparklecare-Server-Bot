25fa122a8e493b5a7c262e8c0b9c24d65ad0f5b8
BurlierEarth7 <j.tollefson@outlook.com>
2024-04-06 16:31:16 -0600

    Turn verification response to Embed verification response is now an embed with buttons to verify or deny Todo: get Deny button to show Modal to add reason

--- commands/moderation/checkVerification.js
+++ commands/moderation/checkVerification.js
@@ -1,4 +1,15 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+//TODO: Get deny Modal to show up, get info from modal and send reason
+const {
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  EmbedBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -15,17 +26,37 @@ module.exports = {
 
   // End of command builder
   async execute(interaction) {
-    var reply = "User's verification status: \n";
     var canVerify = true;
     const user = interaction.options.getUser("user");
+    const channel = client.guilds.cache
+      .get(interaction.guildId)
+      .channels.cache.find(
+        (chan) =>
+          chan.name.toLowerCase() == `ticket-${user.username.toLowerCase()}`
+      );
+    if (!channel) {
+      await interaction.reply({
+        content: "Could not find a ticket channel for this user.",
+        ephemeral: true,
+      });
+      return false;
+    }
+
+    const reply = new EmbedBuilder().setTitle(
+      user.globalName + "'s verification status:"
+    );
     // Check if account is older than 1 week
     if (Date.now() - user.createdAt < 604800000) {
       canVerify = false;
-      reply += "# WARNING\nAccount ***IS*** under 1 week old!.\n";
+      reply.addFields({
+        name: "WARNING",
+        value: "Account ***IS*** under 1 week old!.",
+        inline: true,
+      });
     }
 
-    // Get user's messages in this channel, check for a number
-    const messages = await interaction.channel.messages.fetch({ limit: 100 }); // Get last 100 messages
+    // Get user's messages in ticket channel (name of channel == user name), check for a number
+    const messages = await channel.messages.fetch({ limit: 100 }); // Get last 100 messages
     var age = -1;
     await messages.every(function (message) {
       if (message.author.id == user.id) {
@@ -33,11 +64,12 @@ module.exports = {
           age = parseInt(message.content.replace(/[^0-9]/g, ""));
           if (age < 16) {
             canVerify = false;
-            reply += "## Caution\nUser is **POSSIBLY** under 16 years old.\n";
-          } else if (age > 60) {
-            reply += "### Notice:\nAge gathered may be incorrect, as it is past the age of 60.\n(They might also just be old though)\n";
+            reply.addFields({
+              name: "Caution",
+              value: "User is **POSSIBLY** under 16 years old.",
+              inline: true,
+            });
           }
-
 
           return false; // Stop going through messages
         }
@@ -46,20 +78,103 @@ module.exports = {
     }); // End of messages.every
 
     if (age == -1) {
-      reply +=
-        "## Caution\nCould not find a message from the user containing their age.\n *Are you running the command in the ticket?*\n";
+      reply.addFields({
+        name: "Caution",
+        value: "Could not find a message from the user containing their age.",
+        inline: true,
+      });
       canVerify = false;
     }
 
     if (canVerify) {
-      reply += "User is **LIKELY** eligible for verification.";
+      reply.setFooter({
+        text: "User is **LIKELY** eligible for verification.",
+      });
     } else {
-      reply += "User is **LIKELY NOT** eligible for verification.";
+      reply.setFooter({
+        text: "User is **LIKELY NOT** eligible for verification.",
+      });
     }
 
-    await interaction.reply({
-      content: reply,
-      ephemeral: true,
+    // Add buttons
+    const verify = new ButtonBuilder()
+      .setCustomId("verify")
+      .setLabel("Verify")
+      .setStyle(ButtonStyle.Success);
+
+    const deny = new ButtonBuilder()
+      .setCustomId("deny")
+      .setLabel("Deny")
+      .setStyle(ButtonStyle.Danger);
+
+    const goToTicket = new ButtonBuilder()
+      .setLabel("View Channel")
+      .setURL(
+        `https://discord.com/channels/${interaction.guildId}/${channel.id}`
+      )
+      .setStyle(ButtonStyle.Link);
+
+    const row = new ActionRowBuilder().addComponents(verify, deny, goToTicket);
+
+    const response = await interaction.reply({
+      embeds: [reply],
+      components: [row],
     });
+
+    // Button actions
+    const filter = (i) => i.user.id === interaction.user.id; // Only allow the user who ran the command to interact with the buttons
+    try {
+      const verification = await response.awaitMessageComponent({
+        filter: filter,
+        time: 3_600_000,
+      }); // Wait for 1 hour for a button to be clicked
+
+      if (verification.customId == "verify") {
+        await channel.send("?v " + user.id);
+        await channel.send(
+          "You have been verified! This ticket will be closed manually by a staff member shortly."
+        );
+        await interaction.editReply({
+          content: "User has been verified. Make sure to close the ticket!",
+          components: [],
+        });
+      } else if (verification.customId == "deny") {
+        const denyReason = new ModalBuilder()
+          .setTitle("Deny Reason")
+          .setCustomId("denyReason");
+
+        const rsn = new TextInputBuilder()
+          .setCustomId("reason")
+          .setPlaceholder("Reason for denial")
+          .setStyle(TextInputStyle.Short)
+          .setMaxLength(255);
+        denyReason.addComponents(rsn);
+        await verification.showModal(denyReason);
+        await verification.editReply({
+          content:
+            "User has been denied verification. Make sure to close the ticket!",
+          components: [],
+        });
+      } else if (verification.customId == "denyReason") {
+        const rsn = verification.fields.getTextInputValue("reason");
+        await channel.send(
+          "You have been denied verification. Reason: " + rsn == ""
+            ? "No reason provided."
+            : rsn
+        ); // Send the reason to the user
+        await interaction.editReply({
+          content:
+            "User has been denied verification. Make sure to close the ticket!",
+          components: [],
+        });
+      }
+    } catch (err) {
+      await interaction.editReply({
+        content:
+          "Embed Component's timed out automatically after 1 hour of inactivity. Please run /check again to view again.",
+        components: [],
+      });
+      console.log(err);
+    }
   }, // End of execute
 }; // End of module.exports
